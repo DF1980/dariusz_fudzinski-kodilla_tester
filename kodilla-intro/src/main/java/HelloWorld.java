19003bcb395add58f70c1025230407ad53f8c121
DariuszFudzinski <dariusz.fudzinski@gmail.com>
2022-06-01 13:04:35 +0200

    HelloWorld

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Halo World");
+        System.out.println("Darek");
+        System.out.println(42);
+        System.out.println('F');  }
+}
+
