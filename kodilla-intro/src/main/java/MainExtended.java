08e0f07e0fe5a32767b8079309492cf6531c2961
DariuszFudzinski <dariusz.fudzinski@gmail.com>
2022-06-02 14:18:09 +0200

    MainEXTended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myWord = "Kosmos";
+        int myNumber = 42;
+        double height = 1.91;
+        char myLetter = 'F';
+        boolean isnotGoodProgrammer = true;
+        double pi = 3.14;
+        System.out.println(myWord);
+        System.out.println(myNumber);
+        System.out.println(height);
+        System.out.println(myLetter);
+        System.out.println(isnotGoodProgrammer);
+        System.out.println(pi);
+    }
+}
